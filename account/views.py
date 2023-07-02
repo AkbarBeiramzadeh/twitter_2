b3fb4fc8447edefb1d2595dc1ffbf205d2abca74
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-02 11:49:39 +0330

    feat: import UserRegistrationForm to View

--- account/views.py
+++ account/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegistrationForm
 
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'account/register.html')
+        form = UserRegistrationForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'account/register.html', context=context)
 
     def post(self, request):
         pass
