5f3f5885db26395ae197c45315aeecb1f6489f47
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-03 23:41:30 +0330

    feat: import LoginRequiredMixin

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserRegisterView(View):
@@ -60,8 +61,9 @@ class UserLoginView(View):
         return render(request, self.template_name, {'form': form})
 
 
-class UserLogoutView(View):
-
+class UserLogoutView(LoginRequiredMixin,View):
+# ازث بری از LoginRequiredMixin یعنی :
+# کاربرانی که لاگین کرده اند می توانند به لاگ آوت دسترسی داشته باشند
     def get(self, request):
         logout(request)
         # messages.success(request, 'you logged out successfully', 'success')
