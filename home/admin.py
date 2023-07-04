17e7f9729a58f20dfee663701a5114c8a423e2d1
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-04 15:36:42 +0330

    feat: create class PostAdmin in admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'slug', 'updated')
+    search_fields = ('title',)
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)} # این نوع پر کردن slug فقط در ادمین پنل می باشد.
+    raw_id_fields = ('user',)
