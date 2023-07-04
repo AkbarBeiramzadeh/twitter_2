058f9efabf3e065cc072160184e14b422108dc64
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-04 16:24:06 +0330

    feat: create PostDetailView class

--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, template_name='home/index.html')
+        posts = Post.objects.all()
+        context = {
+            'posts': posts
+        }
+        return render(request, template_name='home/index.html', context=context)
 
-    def post(self, request):
-        return render(request, template_name='home/index.html')
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        return render(request, 'home/detail.html', {'post': post})
