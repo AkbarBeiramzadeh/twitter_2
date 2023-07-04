cd19a45a62cc3e211ff01b1b82c68cd2acb23c7b
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-04 11:57:48 +0330

    feat: added str method

--- home/models.py
+++ home/models.py
@@ -7,5 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=240)
     body = models.TextField()
     slug = models.SlugField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} / {self.title}"
