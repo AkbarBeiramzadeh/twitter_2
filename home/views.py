3435bc083e48837849b01ee9747373ce9c0bddd1
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-05 11:50:04 +0330

    feat: create class PostDeleteView

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(View):
@@ -16,3 +18,14 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you can not delete this post', 'danger')
+        return redirect('home:home')
