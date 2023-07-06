7df77b08952543c169dbba2ebdbcceb18710cba0
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-06 11:43:57 +0330

    feat: create class Comment

--- home/models.py
+++ home/models.py
@@ -4,7 +4,8 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')  # اگر یک یوزر حذف شد پست هایش نیز حذف می شود.
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='posts')  # اگر یک یوزر حذف شد پست هایش نیز حذف می شود.
     title = models.CharField(max_length=240)
     body = models.TextField()
     slug = models.SlugField()
@@ -19,3 +20,15 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.user} / {self.title}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomments')
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
